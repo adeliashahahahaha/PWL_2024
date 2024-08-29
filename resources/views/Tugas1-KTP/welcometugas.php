a6a6d3f2df88209b264db0a1a29729fc111f35ca
adeliashahahahaha <asha.syrn5@gmail.com>
2024-08-29 13:43:54 +0700

    first commit

---
+++ resources/views/Tugas1-KTP/welcometugas.php
@@ -0,0 +1,44 @@
+<html>
+
+<head>
+    <style>
+        .container {
+            max-width: 790px;
+            max-height: 500px auto;
+            margin: 100px;
+            border: 3px solid grey;
+            background-color: #83cefc;
+            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+            border-radius: 15px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <center>
+            <h3>PROVINSI JAWA TIMUR <br> KOTA SURABAYA</h3>
+        </center>
+        <H3>NIK : 3578144505040001</H3>
+        <p>
+            Nama&nbsp;&nbsp;&nbsp;&nbsp;: ADELIA SYAHARANI HERMAWAN <br>
+            Tempat/Tgl Lahir : SURABAYA, 05-05-2004 <br>
+            Jenis Kelamin : PEREMPUAN Gol.Darah : O<br>
+            Alamat : Jl. Manukan Karya A2 No. 8<br>
+            <span style="margin-left: 20px; display: block;">
+                RT/RW : 004/008<br>
+                Kel/Desa : Manukan Kulon<br>
+                Kecamatan : Tandes<br>
+            </span>
+            Agama : ISLAM<br>
+            Status Perkawinanan BELUM KAWIN<br>
+            Pekerjaan : PELAJAR/MAHASISWA<br>
+            Kewarganegaraan : WNI<br>
+            Berlaku Hingga : SEUMUR HIDUP<br>
+        </p>
+    </div>
+</body>
+
+</html>
