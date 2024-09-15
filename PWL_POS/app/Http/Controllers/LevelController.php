89107682095644ed7ea936b7e9787c30959257ae
adeliashahahahaha <asha.syrn5@gmail.com>
2024-09-15 21:12:29 +0700

    [JS3] praktikum 6

--- PWL_POS/app/Http/Controllers/LevelController.php
+++ PWL_POS/app/Http/Controllers/LevelController.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index(){
-        // DB::insert ('insert into m_level(level_kode,level_nama,created_at) values(?,?,?)',['cus','Pelanggan',now()]);
-        // return 'Insert data baru berhasil';
+        DB::insert ('insert into m_level(level_kode,level_nama,created_at) values(?,?,?)',['cus','Pelanggan',now()]);
+        return 'Insert data baru berhasil';
 
         // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer','cus']);
         // return 'Update data berhasil.Jumlah data yang diupdate: ' .$row.'baris';
@@ -17,7 +17,7 @@ class LevelController extends Controller
         // $row = DB::delete('delete from m_level where level_kode = ?',['cus']);
         // return 'Delete data berhasil. Jummlah data yang dihapus:  ' . $row. ' baris';
         
-        $data = DB::select('select * from m_level');
-        return view ('level',['data' => $data]);
+        // $data = DB::select('select * from m_level');
+        // return view ('level',['data' => $data]);
     }
 }
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function() {
@@ -18,5 +19,9 @@ Route::get('/level', [LevelController::class, 'index']);
 //QUERY BUILDER
 
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+//ORM
+
+Route::get('/user', [UserController::class, 'index']);
 
 
