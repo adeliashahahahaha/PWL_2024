1dc9994cc7977abd084e40278065dae311f049be
adeliashahahahaha <asha.syrn5@gmail.com>
2024-09-18 19:43:12 +0700

    [JS4] praktikum 2.4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -76,7 +76,40 @@ class UserController extends Controller
         // return view('user', ['data' => $userCount]);
 
         // -------- [JS4] praktikum 2.4 ----------
-        
+        // $user=UserModel::firstOrCreate(
+        // [
+        //     'username' =>'manager',
+        //     'nama' => 'Manager',
+        // ],);
+        // return view('user', ['data' => $user]);
+        //------------------------------------------
+        // $user=UserModel::firstOrCreate(
+        //     [
+        //         'username' =>'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make ('12345'),
+        //         'level_id' => 2
+        //     ],);
+    
+        // return view('user', ['data' => $user]);
+        //------------------------------------------
+        // $user=UserModel::firstOrNew(
+        //     [
+        //         'username' =>'manager',
+        //         'nama' => 'Manager',
+        //     ],);
+        //     return view('user', ['data' => $user]);
+        //------------------------------------------
+        $user=UserModel::firstOrNew(
+            [
+                'username' =>'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make ('12345'),
+                'level_id' => 3
+            ],);
+            $user->save();
+    
+        return view('user', ['data' => $user]);   
 
     }
 }
