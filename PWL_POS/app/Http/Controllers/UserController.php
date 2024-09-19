a2745adde40fa537ff05a67c2d89de2967a26b5d
adeliashahahahaha <asha.syrn5@gmail.com>
2024-09-19 21:25:31 +0700

    [JS4] praktikum 2.5

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -100,16 +100,59 @@ class UserController extends Controller
         //     ],);
         //     return view('user', ['data' => $user]);
         //------------------------------------------
-        $user=UserModel::firstOrNew(
-            [
-                'username' =>'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make ('12345'),
-                'level_id' => 3
-            ],);
-            $user->save();
+        // $user=UserModel::firstOrNew(
+        //     [
+        //         'username' =>'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make ('12345'),
+        //         'level_id' => 3
+        //     ],);
+        //     $user->save();
     
-        return view('user', ['data' => $user]);   
+        // return view('user', ['data' => $user]);  
+        
+        // -------- [JS4] praktikum 2.5 ----------
+        // $user=UserModel::create(
+        //     [
+        //         'username' =>'manager55',
+        //         'nama' => 'Manager55',
+        //         'password' => Hash::make ('12345'),
+        //         'level_id' => 2
+        //     ]);
+    
+        // $user -> username = 'manager56';
+
+        // $user -> isDirty(); //true
+        // $user -> isDirty('username'); //true
+        // $user -> isDirty('nama'); //false
+        // $user -> isDirty(['nama', 'username']); //true
+
+        // $user -> isClean(); //false
+        // $user -> isClean('username'); //false
+        // $user -> isClean('nama'); //true
+        // $user -> isClean(['nama', 'username']); //false
+
+        // $user -> save();
+
+        // $user -> isDirty(); //true
+        // $user -> isClean(); //false
+        // dd($user->isDirty());
+        //------------------------------------------
+        $user=UserModel::create(
+            [
+                'username' =>'manager11',
+                'nama' => 'Manager11',
+                'password' => Hash::make ('12345'),
+                'level_id' => 2
+            ]);
+    
+        $user -> username = 'manager12';
+
+        $user -> wasChanged(); //true
+        $user -> wasChanged('username'); //true
+        $user -> wasChanged(['username', 'level_id']); //true
+        $user -> wasChanged('nama'); //false
+        $user -> wasChanged(['nama', 'username']); //true
 
     }
 }
