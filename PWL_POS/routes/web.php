9fa92641a1b401f61d8b1007b67600adb47ad03a
adeliashahahahaha <asha.syrn5@gmail.com>
2024-09-28 00:27:48 +0700

    [JS5] praktikum 2

--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function() {
@@ -31,5 +32,7 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/ubah_simpan/{id}', [UserController::class, 'ubah_simpan'])->name('user.ubah_simpan');
 
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/', [WelcomeController::class, 'index']);
 
 
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,16 +6,25 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello()
-    {
-        return 'Hellow World';
-    }
-    // public function greeting(){
-    //     return view('blog.hello', ['name' => 'Adel dari controller']);
+    // public function hello()
+    // {
+    //     return 'Hellow World';
     // }
-    public function greeting(){
-        return view('blog.hello')
-        ->with('name','ADELL')
-        ->with('occupation','Astronaut');
+    // // public function greeting(){
+    // //     return view('blog.hello', ['name' => 'Adel dari controller']);
+    // // }
+    // public function greeting(){
+    //     return view('blog.hello')
+    //     ->with('name','ADELL')
+    //     ->with('occupation','Astronaut');
+    // }
+
+    public function index(){
+        $breadcrumb = (object) [
+            'title' => 'Selamat Datang',
+            'list' => ['Home','Welcome']
+        ];
+        $activeMenu = 'dashboard';
+        return view ('welocme', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
     }
 }
